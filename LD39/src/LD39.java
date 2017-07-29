0c6bab509b26afdca68d2da82f90c434f1af327e
Thomas Barnette <tvbarnet@ncsu.edu>
2017-07-29 10:47:30 -0400

    Initial Commit

---
+++ LD39/src/LD39.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+
+public class LD39 {
+
+	public JFrame frame = new JFrame();
+	
+	
+	public static void main(String[] args) {
+		LD39 theLD = new LD39();
+		theLD.start();
+	}
+	
+	public void start(){
+		
+		
+	}
+
+}
